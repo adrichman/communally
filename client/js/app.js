58a71a7e6c4132b9c3dc74726631fda277034df4
Adam D Richman <ar@adamrichman.com>
2014-02-11 06:54:56 -0800

    form submits but is creating duplicate submissions

--- client/js/app.js
+++ client/js/app.js
@@ -4,45 +4,45 @@ communallyApp.controller("topicsController", ["$scope", "$firebase",
   function($scope, $firebase){
     var ref = new Firebase("https://communally.firebaseio.com/Topic");
     $scope.topics = $firebase(ref);
-    console.log($scope.topics);
   }
 ]);
 communallyApp.controller("categoriesController", ["$scope", "$firebase",
   function($scope, $firebase){
     var ref = new Firebase("https://communally.firebaseio.com/TopicCategory");
     $scope.categories = $firebase(ref);
-    console.log($scope.categories);
   }
 ]);
 communallyApp.controller("reportPropertiesController", ["$scope", "$firebase",
   function($scope, $firebase){
     var ref = new Firebase("https://communally.firebaseio.com/ReportProperty");
     $scope.properties = $firebase(ref);
-    console.log($scope.properties);
   }
 ]);
 communallyApp.controller("reportsListController", ["$scope", "$firebase",
   function($scope, $firebase){
     var ref = new Firebase("https://communally.firebaseio.com/Report");
     $scope.reports = $firebase(ref);
-    console.log($scope.reports);
   }
 ]);
 communallyApp.controller("newReportController", ["$scope", "$firebase",
   function($scope, $firebase){
     var ref = new Firebase("https://communally.firebaseio.com/Report");
+    var catRef = new Firebase("https://communally.firebaseio.com/TopicCategory");
+    var propRef = new Firebase("https://communally.firebaseio.com/ReportProperty");
+    $scope.categories = $firebase(catRef);
+    $scope.props = $firebase(propRef);
     $scope.reports = $firebase(ref);
-    $scope.newReport = function(){
-      // $scope.reports.$add($scope.newReport);
-      ref.update({
+    $scope.addReport = function(){
+      $scope.reports.$add({
         location: $scope.location_input,
         summary:  $scope.summary_input,
         category: $scope.category_select,
         property: $scope.property_select,
-        comments: $scope.comments_input
+        comments: $scope.comments_input,
+        created_at: new Date(Date.now())
       });
 
-      $scope.newReport = "";
     }
+    $scope.newReport = "";
   }
 ]);
