577bbc30cba877bd3ebfafe402168d134844a95f
Adam D Richman <ar@adamrichman.com>
2014-02-11 05:28:35 -0800

    angular and firebase. displays records and categories. populates fields. form needs to be hooked up for submission.

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,48 @@
-// var ref = new Firebase("https://communally.firebaseio.com/").limit(10);
+var communallyApp = angular.module("communallyApp", ["firebase"])
+
+communallyApp.controller("topicsController", ["$scope", "$firebase",
+  function($scope, $firebase){
+    var ref = new Firebase("https://communally.firebaseio.com/Topic");
+    $scope.topics = $firebase(ref);
+    console.log($scope.topics);
+  }
+]);
+communallyApp.controller("categoriesController", ["$scope", "$firebase",
+  function($scope, $firebase){
+    var ref = new Firebase("https://communally.firebaseio.com/TopicCategory");
+    $scope.categories = $firebase(ref);
+    console.log($scope.categories);
+  }
+]);
+communallyApp.controller("reportPropertiesController", ["$scope", "$firebase",
+  function($scope, $firebase){
+    var ref = new Firebase("https://communally.firebaseio.com/ReportProperty");
+    $scope.properties = $firebase(ref);
+    console.log($scope.properties);
+  }
+]);
+communallyApp.controller("reportsListController", ["$scope", "$firebase",
+  function($scope, $firebase){
+    var ref = new Firebase("https://communally.firebaseio.com/Report");
+    $scope.reports = $firebase(ref);
+    console.log($scope.reports);
+  }
+]);
+communallyApp.controller("newReportController", ["$scope", "$firebase",
+  function($scope, $firebase){
+    var ref = new Firebase("https://communally.firebaseio.com/Report");
+    $scope.reports = $firebase(ref);
+    $scope.newReport = function(){
+      // $scope.reports.$add($scope.newReport);
+      ref.update({
+        location: $scope.location_input,
+        summary:  $scope.summary_input,
+        category: $scope.category_select,
+        property: $scope.property_select,
+        comments: $scope.comments_input
+      });
+
+      $scope.newReport = "";
+    }
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-
-var port = 8080;
 var ip = "127.0.0.1";
 
 app.configure(function(){
@@ -12,26 +10,13 @@ app.configure(function(){
 var routes = {
   "/"                                                  : {'path':'client/index.html',                                       'type':'text/html'},
   "/index.html"                                        : {'path':'client/index.html',                                       'type':'text/html'},
-  "/js/main.js"                                        : {'path':'client/js/main.js',                                       'type':'text/javascript'},
-  "/app/Models/AppModel.js"                            : {'path':'client/app/Models/AppModel.js',                           'type':'text/javascript'},
-  "/app/Views/AppView.js"                              : {'path':'client/app/Views/AppView.js',                             'type':'text/javascript'},
-  "/app/Views/TopicCategoryView.js"                    : {'path':'client/app/Views/TopicCategoryView.js',                             'type':'text/javascript'},
-  "/app/Views/ReportView.js"                           : {'path':'client/app/Views/ReportView.js',                             'type':'text/javascript'},
-  "/js/app.js"                                         : {'path':'client/js/app.js',                                        'type':'text/javascript'},
-  "/app/Models/Topic.js"                               : {'path':'client/app/Models/Topic.js',                              'type':'text/javascript'},
-  "/app/Models/Topics.js"                              : {'path':'client/app/Models/Topics.js',                            'type':'text/javascript'},
-  "/app/Models/Report.js"                              : {'path':'client/app/Models/Report.js',                            'type':'text/javascript'},
-  "/app/Models/Reports.js"                              : {'path':'client/app/Models/Reports.js',                            'type':'text/javascript'},
-  "/app/Models/TopicCategory.js"                       : {'path':'client/app/Models/TopicCategory.js',                            'type':'text/javascript'},
-  "/app/Views/TopicView.js"                            : {'path':'client/app/Views/TopicView.js',                           'type':'text/javascript'},
-  "/app/Views/TopicsView.js"                           : {'path':'client/app/Views/TopicsView.js',                          'type':'text/javascript'},
+  "/js/app.js"                                         : {'path':'client/js/angular.js',                                    'type':'text/javascript'},
+  "/bower_components/angular/angular.min.js.map"       : {'path':'client/bower_components/angular/angular.min.js.map',                            'type':'text/javascript'},
+  "/bower_components/angular/angular.js"               : {'path':'client/bower_components/angular/angular.js',                    'type':'text/javascript'},
+  "/js/angular.js"                                     : {'path':'client/js/angular-route.js',                              'type':'text/javascript'},
+  "/js/angular-route.js"                               : {'path':'client/js/app.js',                                        'type':'text/javascript'},
   "/css/bootstrap.css"                                 : {'path':'client/css/slate/bootstrap.css',                          'type':'text/css'},
-  "/css/normalize.css"                                 : {'path':'client/css/normalize.css',                                'type':'text/css'},
-  // "/css/bootstrap.css.map"                             : {'path':'client/css/bootstrap.css.map',                            'type':'text/css'},
-  "/bower_components/backbone/backbone-min.js"         :  {'path':'client/bower_components/backbone/backbone-min.js',       'type':'text/javascript'},
-  "/bower_components/backbone/backbone.js"             :  {'path':'client/bower_components/backbone/backbone.js',           'type':'text/javascript'},
-  "/bower_components/backbone/backbone-min.map"         :  {'path':'client/bower_components/backbone/backbone-min.map',       'type':'text/javascript'},
-  "/bower_components/backfire/backbone-firebase.min.js": {'path':'client/bower_components/backfire/backbone-firebase.min.js','type':'text/javascript'},
+  "/css/bootstrap.css.map"                             : {'path':'client/css/bootstrap.css.map',                            'type':'text/css'},
   "/bower_components/underscore/underscore-min.js"     : {'path':'client/bower_components/underscore/underscore-min.js',    'type':'text/javascript'},
   "/bower_components/underscore/underscore-min.map"    : {'path':'client/bower_components/underscore/underscore-min.map',   'type':'text/javascript'},
   "/bower_components/jquery/jquery.min.js"             : {'path':'client/bower_components/jquery/jquery.min.js',            'type':'text/javascript'},
